bbb021ebb6e7c8d2d485a8622eaa3e4d93a49e00
Vanlightly <vanlightly@gmail.com>
2021-05-13 09:02:42 -0700

    Added tunable msg loss to the node itself

--- systems/src/main/java/com/vanlightly/bookkeeper/Config.java
+++ systems/src/main/java/com/vanlightly/bookkeeper/Config.java
@@ -1,5 +1,10 @@
 package com.vanlightly.bookkeeper;
 
 public class Config {
-    public final static boolean CheckInvariants = false;
+    public final static boolean CheckInvariants = true;
+    public final static boolean RecoveryReadsFence = false;
+    public final static int AddReadSpreadMs = 0;
+    public final static boolean LoseFencingMsg = true;
+    public final static double MsgLoss = 0.0;
+
 }
--- systems/src/main/java/com/vanlightly/bookkeeper/network/StdInOutNetwork.java
+++ systems/src/main/java/com/vanlightly/bookkeeper/network/StdInOutNetwork.java
@@ -1,17 +1,27 @@
 package com.vanlightly.bookkeeper.network;
 
+import com.vanlightly.bookkeeper.Config;
+import com.vanlightly.bookkeeper.util.LogManager;
+import com.vanlightly.bookkeeper.util.Logger;
+
+import java.util.Random;
 import java.util.Scanner;
+import java.util.UUID;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class StdInOutNetwork implements NetworkIO {
 
-    Scanner scanner;
+    private Logger logger = LogManager.getLogger(this.getClass().getSimpleName());
+    private Scanner scanner;
     private Lock lock;
+    private Random rand;
+
 
     public StdInOutNetwork() {
         scanner = new Scanner(System.in);
         lock = new ReentrantLock();
+        rand = new Random(UUID.randomUUID().hashCode());
     }
 
 
@@ -23,12 +33,18 @@ public class StdInOutNetwork implements NetworkIO {
     @Override
     public String readNext() {
         String line = scanner.nextLine();
-        //System.err.println(line);
         return line;
     }
 
     @Override
     public void write(String msg) {
+        if (Config.MsgLoss > 0) {
+            if (rand.nextDouble() <= Config.MsgLoss) {
+                logger.logDebug("DROPPED: " + msg);
+                return;
+            }
+        }
+
         lock.lock();
         try {
             System.out.println(msg);
